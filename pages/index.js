5549648d527cd0fe53c65474536254acf61db1aa
Haifeng Zhu <haifeng.zhu@iherb.com>
2022-04-26 20:57:53 +0800

    add customized config

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import { Animated, Basic, bounce, Combined } from "../shared/styles";
 
 const Home = () => (
   <div className="flex gap-8 text-2xl">
-    <div className="w-[200px] h-[200px] bg-red-600">box1</div>
-    <div className="w-[200px] h-[200px] bg-red-700">box2</div>
+    <div className="w-[200px] h-[200px] bg-red-600 dark-blue">box1</div>
+    <div className="w-[200px] h-[200px] bg-blue-dark">box2</div>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,78 @@
+const plugin = require("tailwindcss/plugin");
+const blue = {
+  light: "#bbdefb",
+  standard: "#2196F3",
+  dark: "#1976d2",
+};
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        gray: {
+          100: "#FAFAFA",
+          200: "#F5F5F5",
+          300: "#E0E0E0",
+          400: "#CCCCCC",
+          500: "#949494",
+          600: "#707070",
+          700: "#333333",
+          800: "#101010",
+        },
+        green: {
+          light: "#E2FFDB",
+          standard: "#54A300",
+          dark: "#458500",
+        },
+        orange: {
+          light: "#FBF1EA",
+          standard: "#F38A00",
+          dark: "#D47500",
+        },
+        red: {
+          light: "#ffcdd2",
+          standard: "#f44336",
+          dark: "#D32F2F",
+        },
+        yellow: {
+          light: "#FFF9C4",
+          standard: "#FFEA00",
+          dark: "#8A6702",
+        },
+        blue: {
+          light: "#D6EAFF",
+          standard: "#2196F3",
+          dark: "#126CC5",
+        },
+        lemon: {
+          light: "#FFF5C2",
+          standard: "#FFEF9A",
+          dark: "#FFD644",
+        },
+        mango: {
+          light: "#ffe6b0",
+          standard: "#feca57",
+          dark: "#ff9f43",
+        },
+        charcoal: "#222f3e",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".outlined-disabled": {
+          color: "#f5f5f5 !important",
+          backgroundColor: "#ccc !important",
+        },
+        ".dark-blue": {
+          color: blue.dark,
+        },
+      });
+    }),
+  ],
 };
